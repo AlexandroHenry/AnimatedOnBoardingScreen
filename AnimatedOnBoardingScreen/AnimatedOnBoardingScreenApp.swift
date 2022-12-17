c8d4d7b57ad04217c42b5a3e0947a49fc56a7b58
alaxhenry <alaxhenry0121@gmail.com>
2022-12-17 13:29:51 +0900

    Initial Commit

---
+++ AnimatedOnBoardingScreen/AnimatedOnBoardingScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedOnBoardingScreenApp.swift
+//  AnimatedOnBoardingScreen
+//
+//  Created by Seungchul Ha on 2022/12/17.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedOnBoardingScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
